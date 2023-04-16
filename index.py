f559e38f04b568262ebb5a46d709037c786e3e0b
rangapin <rangapin@gmx.com>
2023-04-16 19:02:51 +0200

    day one

---
+++ index.py
@@ -0,0 +1,8 @@
+print("Welcome to the band generator")
+
+city = input("What city were you born in?\n")
+
+pet = input("What is the name of your pet?\n") 
+
+
+print("The name of your band is " + city + " " + pet)
